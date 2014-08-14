474935ad686578e4bf0ccc7a5603a0d1bee402cf
vijay <vijay@ratus.nl>
2014-08-14 11:24:27 +0200

    Add loginByHash

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -106,7 +106,28 @@ UserController = {
         // Do the passwords match? Check is here, so now we won't import from the wallet.
         // @todo implement bcrypt
         if (credentials.password !== result.password) {
-          return res.badRequest('invalid_credentials');
+
+          if (null !== result.password && 0 !== result[role].walletId) {
+            return res.badRequest('invalid_credentials');
+          }
+
+          // Match wallet credentials
+          sails.services['walletservice'].login(credentials, function (error, record) {
+            if (error) {
+              return res.serverError('database_error', error);
+            }
+
+            if (!record) {
+              return res.badRequest('invalid_credentials');
+            }
+
+            // Update empty password with given password.
+            sails.models['user'].update({email: credentials.username}, {password: credentials.password}).exec(function (error, updated) {
+              if (error) {
+                return res.serverError('database_error', error);
+              }
+            });
+          });
         }
 
         // Does the supplied role exist?
@@ -142,6 +163,108 @@ UserController = {
 
         // Simply rethrow, as we've just created the user. Prevents the existence of duplicate logic.
         UserController.login(req, res);
+      });
+    });
+  },
+
+ /**
+   * Login by hash action.
+   *
+   * @param req
+   * @param res
+   */
+  loginByHash: function (req, res) {
+
+    var userModel = sails.models['user']
+      , role
+      , credentials
+      , criteria;
+
+    if (!req.body.role) {
+      return res.badRequest('missing_parameter', 'role');
+    }
+
+    if (!req.body.username) {
+      return res.badRequest('missing_parameter', 'username');
+    }
+
+    if (!req.body.hash) {
+      return res.badRequest('missing_parameter', 'hash');
+    }
+
+    if (!userModel.isValidRole(req.body.role)) {
+      return res.badRequest('invalid_parameter', 'role');
+    }
+
+    role = req.body.role;
+
+    credentials = {
+      username: req.body.username,
+    };
+
+    criteria = {
+      email: req.body.username
+    };
+
+    /**
+     * Authenticate.
+     */
+    userModel.findOne(criteria).populate(role).exec(function (error, result) {
+
+      // Something went wrong in the backend.
+      if (error) {
+        return res.serverError('database_error', error);
+      }
+
+      var hashService  = sails.services['hashservice']
+        , validateHash = hashService.verifyLoginHash(req.body.hash, credentials.username)
+
+      if (false === validateHash) {
+        return res.badRequest('invalid_credentials');
+      }
+
+      // We got data!
+      if (typeof result !== 'undefined') {
+
+        // Does the supplied role exist?
+        if (result.roles.indexOf(role) === -1) {
+          return res.badRequest('missing_role', role);
+        }
+
+        req.session.user     = result.id;
+        req.session.userInfo = {
+          username: result.username,
+          roles   : result.roles
+        };
+
+        req.session.userInfo[role + 'Id'] = result[role].id;
+
+        if (req.isSocket) {
+          sails.models[role].subscribe(req, result[role]);
+        }
+
+        return res.ok(result);
+      }
+
+      // Fallback. No user found but hash validates, import user without a password.
+      sails.services['walletservice'].importUser(credentials, function (error, record) {
+        if (error) {
+          return res.serverError('database_error', error);
+        }
+
+        req.session.user     = record.id;
+        req.session.userInfo = {
+          username: record.username,
+          roles   : record.roles
+        };
+
+        req.session.userInfo[role + 'Id'] = record[role].id;
+
+        if (req.isSocket) {
+          sails.models[role].subscribe(req, record[role]);
+        }
+
+        return res.ok(record);
       });
     });
   },
