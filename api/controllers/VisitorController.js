68bd9ed17b8ea6d99ce6155c2c37b7d8086ce478
RWOverdijk <r.w.overdijk@gmail.com>
2014-08-19 14:18:08 +0200

    Added username check to setUsername

--- api/controllers/VisitorController.js
+++ api/controllers/VisitorController.js
@@ -6,23 +6,40 @@
  */
 
 module.exports = {
-  setUsername : function(req, res) {
+  setUsername: function (req, res) {
     if (!req.body.username) {
       return res.badRequest('missing_parameter', 'username');
     }
-    sails.models.user.update(req.session.user, {username: req.body.username}).exec(function(error) {
+
+    sails.services.userservice.usernameAvailable(req.body.username, function (error, available) {
       if (error) {
         return res.serverError('database_error', error);
       }
 
-      sails.models.visitor.update(req.session.userInfo.visitorId, {username: req.body.username}).exec(function(error) {
+      if (!available) {
+        return res.badRequest('username_exists', req.body.username);
+      }
+
+      sails.models.user.update(req.session.user, {username: req.body.username}).exec(function (error) {
         if (error) {
+          var invalid = error.invalidAttributes;
+
+          if (invalid && invalid.username && invalid.username[0].rule === 'regex') {
+            return res.badRequest('invalid_parameter', 'username');
+          }
+
           return res.serverError('database_error', error);
         }
 
-        req.session.userInfo.username = req.body.username;
+        sails.models.visitor.update(req.session.userInfo.visitorId, {username: req.body.username}).exec(function (error) {
+          if (error) {
+            return res.serverError('database_error', error);
+          }
 
-        res.ok();
+          req.session.userInfo.username = req.body.username;
+
+          res.ok();
+        });
       });
     });
   }
