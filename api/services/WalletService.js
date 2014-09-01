93299b79c66210314632dc6f95e97f456424d16c
RWOverdijk <r.w.overdijk@gmail.com>
2014-09-01 11:19:07 +0200

    Added object support, refactor

--- api/services/WalletService.js
+++ api/services/WalletService.js
@@ -5,7 +5,13 @@ module.exports = {
 
   importUser: function (credentials, callback) {
 
-    sails.models.wallet.findUser(credentials.username, function (error, user) {
+    var queryUser = credentials.username;
+
+    if (credentials.email) {
+      queryUser = credentials.email;
+    }
+
+    sails.models.wallet.findUser(queryUser, function (error, user) {
       if (error) {
         return callback(error);
       }
@@ -18,13 +24,21 @@ module.exports = {
         walletId   : user.id,
         credits    : user.credits,
         partnerInfo: user.reg_promotor_info,
-        partnerCode: user.partner_code
+        partnerCode: user.partner_code ? user.partner_code : null
       }, newUserValues = {
         email        : credentials.username,
         emailVerified: !!user.email_verified,
+        object       : credentials.object,
         password     : credentials.password || null, // @see UserController.loginByHash
         visitor      : newVisitorValues
       };
+
+      if (credentials.email && credentials.username) {
+        newUserValues.email = credentials.email;
+        newUserValues.username = credentials.username;
+        newUserValues.object = credentials.object;
+        newVisitorValues.username = credentials.username;
+      }
 
       sails.models.user.register(newUserValues, function (error, newUser) {
         if (error) {
@@ -36,11 +50,21 @@ module.exports = {
     });
   },
 
-  login: function (credentials, callback) {
-    var walletUrl = sails.config.userSync.backupAuthenticationUrl,
-        self = this;
+  request: function (action, parameters, callback) {
+    var walletUrl = sails.config.wallet.walletAPIUrl;
 
-    request.post(walletUrl, {form: extend({action: 'login'}, credentials)}, function (error, response, body) {
+    if (typeof parameters === 'function') {
+      callback = parameters;
+      parameters = {};
+    }
+
+    if (parameters.form) {
+      parameters.form.action = action;
+    } else {
+      parameters.action = action;
+    }
+
+    request.post(walletUrl, parameters, function (error, response, body) {
       var responseData;
 
       if (error) {
@@ -53,7 +77,75 @@ module.exports = {
         return callback(error);
       }
 
-      if (!responseData.ok) {
+      callback(null, responseData);
+    });
+  },
+
+  register: function (credentials, callback) {
+    var walletAccount = _.clone(credentials),
+        self = this;
+
+    walletAccount.termsAgreed = 1;
+    walletAccount.username = walletAccount.email;
+
+    delete walletAccount.email;
+    delete walletAccount.object;
+
+    this.request('register', {form: walletAccount}, function (error, response) {
+      if (error) {
+        return callback(error);
+      }
+
+      /**
+       * Sorry for this. It's a bit of logic to "purify" what the wallet returns.
+       * Errors get converted to the format sails uses.
+       */
+      if (response.errors.length) {
+        var walletError = response.errors[0],
+            errorType,
+            errorAttribute,
+            errorObject;
+
+        if (walletError.code === 'account_exists') {
+          return callback(null, false);
+        }
+
+        if (walletError.code === 'email_not_valid') {
+          errorType = 'email';
+          errorAttribute = errorType;
+        } else {
+          errorType = walletAccount.password.length < 5 ? 'minLength' : 'maxLength';
+          errorAttribute = 'password';
+        }
+
+        errorObject = {
+          error            : 'E_VALIDATION',
+          model            : 'User',
+          invalidAttributes: {}
+        };
+
+        errorObject.invalidAttributes[errorAttribute] = [{rule: errorType}];
+
+        return callback(errorObject);
+      }
+
+      if (!response.ok) {
+        return callback(null, false);
+      }
+
+      self.importUser(credentials, callback);
+    });
+  },
+
+  login: function (credentials, callback) {
+    var self = this;
+
+    this.request('login', {form: credentials}, function (error, response) {
+      if (error) {
+        return callback(error);
+      }
+
+      if (!response.ok) {
         return callback(null, false);
       }
 
