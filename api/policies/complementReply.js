7d46a9cc0a2f286bcd6db0d7e9fcf337a7c8d4c4
RWOverdijk <r.w.overdijk@gmail.com>
2014-09-09 15:00:38 +0200

    Get data from policies for track

--- api/policies/complementReply.js
+++ api/policies/complementReply.js
@@ -1,4 +1,4 @@
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   if (!req.body.thread) {
     return res.badRequest('missing_parameter', 'thread');
   }
@@ -7,14 +7,36 @@ module.exports = function(req, res, next) {
     return res.badRequest('missing_parameter', 'body');
   }
 
-  sails.models['message'].findOne({thread: req.body.thread}).exec(function(error, data) {
+  sails.models['message'].findOne({thread: req.body.thread}).exec(function (error, data) {
     if (error) {
       return res.serverError(error);
+    }
+
+    if (!req.trackthis) {
+      req.trackthis = {};
+    }
+
+    // Fetch TO. If to !== visitor, set producer.
+    if (data.to !== req.session.user) {
+      req.trackthis.producer = data;
     }
 
     req.body.to = data.to === req.session.user ? data.from : data.to;
     req.body.from = req.session.user;
 
-    next();
+    if ('visitor' !== req.session.userInfo.authenticatedRole) {
+      return next();
+    }
+
+    sails.services.userservice.getUser(req.body.to, function (error, user) {
+      if (error) {
+        return next(error);
+      }
+
+      req.trackthis.producer = user;
+      req.trackthis.extraUnique = 'reply';
+
+      next();
+    });
   });
 };
