d7622d1bcc99dad8ab67832420f3664ac32cb550
RWOverdijk <r.w.overdijk@gmail.com>
2014-09-10 13:22:54 +0200

    Header content type check

--- test/unit/controllers/UserController.test.js
+++ test/unit/controllers/UserController.test.js
@@ -11,7 +11,7 @@ describe('UserController', function () {
         .get('/user/username/' + testUserId)
         .set('Content-Type', 'application/json')
         .set('X-Object-Host', 'api.islive.io')
-        //.expect('Content-Type', /json/)
+        .expect('Content-Type', /json/)
         .expect(function (res) {
           assert.property(res.body, 'username', 'Username returned');
           assert.strictEqual(res.body.username, 'fixturetest', 'Username equals "fixturetest"');
