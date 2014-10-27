06c74394ffcdf1cd76907efbbe2b2ba6d86ff50a
RWOverdijk <r.w.overdijk@gmail.com>
2014-10-27 09:53:17 +0100

    Added confirm for systems

--- api/controllers/SystemController.js
+++ api/controllers/SystemController.js
@@ -1,5 +1,26 @@
+var requestHelpers = require('request-helpers');
+
 module.exports = {
-  totalConnections : function (req, res) {
+  totalConnections: function (req, res) {
     res.ok({sessions: Object.keys(sails.io.sockets.sockets).length});
+  },
+  verify          : function (req, res) {
+    requestHelpers.pickParams(['user', 'type'], req, function (error, params) {
+      if (error) {
+        return res.negotiate(error);
+      }
+
+      var newValue = {};
+
+      newValue[(params.type === 'email' ? 'email' : 'notificationEmail') + 'Verified'] = true;
+
+      sails.models.user.update(params.user, newValue, function (error) {
+        if (error) {
+          return res.negotiate(error);
+        }
+
+        res.ok();
+      });
+    });
   }
 };
