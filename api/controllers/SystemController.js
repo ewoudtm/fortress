15841628afb563a8fd59e8cc2f58347406dc20e0
RWOverdijk <r.w.overdijk@gmail.com>
2014-10-27 10:20:11 +0100

    Extend verify functionality for wallet

--- api/controllers/SystemController.js
+++ api/controllers/SystemController.js
@@ -5,22 +5,51 @@ module.exports = {
     res.ok({sessions: Object.keys(sails.io.sockets.sockets).length});
   },
   verify          : function (req, res) {
-    requestHelpers.pickParams(['user', 'type'], req, function (error, params) {
-      if (error) {
-        return res.negotiate(error);
-      }
+    var requiredProperties = [
+      'type',
+      {required: false, param: 'user'},
+      {required: false, param: 'walletUser'}
+    ];
 
+    function verify (id, field) {
       var newValue = {};
 
-      newValue[(params.type === 'email' ? 'email' : 'notificationEmail') + 'Verified'] = true;
+      newValue[field + 'Verified'] = true;
 
-      sails.models.user.update(params.user, newValue, function (error) {
+      sails.models.user.update(id, newValue, function (error) {
         if (error) {
           return res.negotiate(error);
         }
 
         res.ok();
       });
+    }
+
+    requestHelpers.pickParams(requiredProperties, req, function (error, params) {
+      if (error) {
+        return res.negotiate(error);
+      }
+
+      var field = params.type === 'email' ? 'email' : 'notificationEmail';
+
+      // Update by Fortress ID (regular users)
+      if (params.user) {
+        return verify(params.user, field);
+      }
+
+      // Update by wallet ID (legacy)
+      if (params.walletUser) {
+        return sails.models.visitor.findOne({walletId: params.walletUser}, function (error, visitor) {
+          if (error) {
+            return res.negotiate(error);
+          }
+
+          verify(visitor.user, field);
+        });
+      }
+
+      // All failed, error.
+      res.negotiate('missing_parameter', 'user or wallet-user');
     });
   }
 };
--- config/routes.js
+++ config/routes.js
@@ -22,51 +22,51 @@
 
 module.exports.routes = {
   // MessageController
-  'post /message'                 : 'MessageController.create',
-  'get /message/inbox'            : 'MessageController.inbox',
-  'put /message/mark-read'        : 'MessageController.markRead',
-  'get /message/unread'           : 'MessageController.unread',
+  'post /message'                : 'MessageController.create',
+  'get /message/inbox'           : 'MessageController.inbox',
+  'put /message/mark-read'       : 'MessageController.markRead',
+  'get /message/unread'          : 'MessageController.unread',
 
   // ThreadController
-  'get /thread'                   : 'ThreadController.find',
-  'put /thread/mark-read'         : 'ThreadController.markRead',
-  'get /thread/thread-count'      : 'ThreadController.getThreadCount',
-  'get /thread/:id'               : 'ThreadController.findonesimple',
+  'get /thread'                  : 'ThreadController.find',
+  'put /thread/mark-read'        : 'ThreadController.markRead',
+  'get /thread/thread-count'     : 'ThreadController.getThreadCount',
+  'get /thread/:id'              : 'ThreadController.findonesimple',
 
   // UserController
-  'put /user/:id'                 : 'UserController.update',
-  'get /user/:id/unsubscribe'     : 'UserController.unsubscribe',
-  'get /user/:id/verify/:type'    : 'UserController.verify', // /user/123/verify/notification-email
-  'get /user/logout'              : 'UserController.logout',
-  'post /user/login'              : 'UserController.login',
-  'post /user/login-by-hash'      : 'UserController.loginByHash',
-  'get  /user/login-by-hash'      : 'UserController.loginByHash',
-  'get /user/username/:id'        : 'UserController.getUsername',
-  'post /user/username-available' : 'UserController.usernameAvailable',
-  'get /user/identity/:role?'     : 'UserController.getIdentity',
+  'put /user/:id'                : 'UserController.update',
+  'get /user/:id/unsubscribe'    : 'UserController.unsubscribe',
+  'get /user/:id/verify/:type'   : 'UserController.verify', // /user/123/verify/notification-email
+  'get /user/logout'             : 'UserController.logout',
+  'post /user/login'             : 'UserController.login',
+  'post /user/login-by-hash'     : 'UserController.loginByHash',
+  'get  /user/login-by-hash'     : 'UserController.loginByHash',
+  'get /user/username/:id'       : 'UserController.getUsername',
+  'post /user/username-available': 'UserController.usernameAvailable',
+  'get /user/identity/:role?'    : 'UserController.getIdentity',
 
   // VisitorController
-  'get  /visitor/register'        : 'VisitorController.register',
-  'post /visitor/register'        : 'VisitorController.register',
-  'put /visitor/username'         : 'VisitorController.setUsername', // @see policies/resolveVisitorIdentity
-  'put /visitor/:id?'             : 'VisitorController.update',
-  'get /visitor/identity'         : 'VisitorController.find',        // @see policies/resolveVisitorIdentity
+  'get  /visitor/register'       : 'VisitorController.register',
+  'post /visitor/register'       : 'VisitorController.register',
+  'put /visitor/username'        : 'VisitorController.setUsername', // @see policies/resolveVisitorIdentity
+  'put /visitor/:id?'            : 'VisitorController.update',
+  'get /visitor/identity'        : 'VisitorController.find',        // @see policies/resolveVisitorIdentity
 
   // PerformerController
-  'get /performer/count'          : 'PerformerController.count',
-  'get /performer/:username'      : 'PerformerController.findonesimple',
+  'get /performer/count'         : 'PerformerController.count',
+  'get /performer/:username'     : 'PerformerController.findonesimple',
 
   // ObjectController
-  'get /object/:id?'              : 'ObjectController.find',
-  'post /object'                  : 'ObjectController.create',
-  'put /object/:id'               : 'ObjectController.update',
-  'delete /object/:id'            : 'ObjectController.destroy',
+  'get /object/:id?'             : 'ObjectController.find',
+  'post /object'                 : 'ObjectController.create',
+  'put /object/:id'              : 'ObjectController.update',
+  'delete /object/:id'           : 'ObjectController.destroy',
 
   // Misc
-  'get /connect/getcookie'        : 'ConnectController.getCookie',
-  'get /connect/safari-getcookie' : 'ConnectController.safariGetCookie',
+  'get /connect/getcookie'       : 'ConnectController.getCookie',
+  'get /connect/safari-getcookie': 'ConnectController.safariGetCookie',
 
   // System
-  'get /system/total-connections' : 'SystemController.totalConnections',
-  'get /system/verify/:type/:user': 'SystemController.verify'
+  'get /system/total-connections': 'SystemController.totalConnections',
+  'get /system/verify/:type'     : 'SystemController.verify'
 };
