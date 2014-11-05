5e69c7395b328774916f505de55b9a3fc97790bd
RWOverdijk <r.w.overdijk@gmail.com>
2014-11-05 15:00:42 +0100

    added sendNotifications policy

---
+++ api/policies/sendNotifications.js
@@ -0,0 +1,47 @@
+var requestHelpers     = require('request-helpers'),
+    requiredProperties = [
+      {required: false, param: 'email'},
+      {required: false, param: 'notificationEmail'}
+    ];
+
+module.exports = function (req, res, next) {
+  requestHelpers.pickParams(requiredProperties, req, function (error, params) {
+    if (error) {
+      return next({error: 'missing_parameter', message: error});
+    }
+
+    if (params.notificationEmail) {
+      sendChangedNotification('notification_email', params.notificationEmail);
+    }
+
+    if (params.email) {
+      sendChangedNotification('email', params.email);
+    }
+
+    if (!params.email && !params.notificationEmail) {
+      return next();
+    }
+
+    function sendChangedNotification (type, newValue) {
+      var notificationService = sails.services.notificationservice,
+          field = type === 'email' ? 'email' : 'notificationEmail',
+          userMock = {
+            id      : req.session.user,
+            object  : req.session.userInfo.objectId || req.object.id,
+            mailable: true,
+            email   : req.session.userInfo.email
+          };
+
+      userMock[field]              = newValue;
+      userMock[field + 'Verified'] = true;
+
+      req.session.userInfo.roles.forEach(function (role) {
+        userMock[role] = req.session.userInfo[role + 'Id'];
+      });
+
+      notificationService.send(type + '_changed', userMock, {
+        verificationHash: sails.services.userservice.generateHash(userMock, 'verify.' + field)
+      }, next);
+    }
+  })
+};
