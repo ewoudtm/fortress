50b0b28ccdafbd0c5ce8200be2d17da3843eda6b
Peter Pal Koszta <pp.koszta@gmail.com>
2014-11-27 14:34:48 +0100

    Fixed sendNotification test missing status after lift in test mode

--- test/unit/policies/sendNotifications.test.js
+++ test/unit/policies/sendNotifications.test.js
@@ -34,7 +34,8 @@ describe('sendNotifications', function () {
 
             assert.deepEqual(response.body, {
               error      : 'not_implemented',
-              description: 'This feature hasn\'t been implemented yet.'
+              description: 'This feature hasn\'t been implemented yet.',
+              status: 500
             }, 'Did not get the expected error response.');
 
             server.close();
