05a909c99e3d8506b6ea2e9f7920bdbc3c447814
RWOverdijk <r.w.overdijk@gmail.com>
2014-12-15 09:16:21 +0100

    Clean, and default config

--- config/wallet.js
+++ config/wallet.js
@@ -1,13 +1,4 @@
 module.exports.wallet = {
-  enabled: false,
-
-  /**
-   * --Request::POST--
-   * username : string
-   * password : string
-   * action   : string
-   * -----------------
-   */
   apiUrl    : 'http://mysecurewallet.nl/payment/islive/ajax/wallet',
   programId : 2473
 };
