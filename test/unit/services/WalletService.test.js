6e1d1198dafbb58e61620aad8e68e482ea8403e6
RWOverdijk <r.w.overdijk@gmail.com>
2014-12-15 13:02:09 +0100

    Added programId tests

--- test/unit/services/WalletService.test.js
+++ test/unit/services/WalletService.test.js
@@ -25,6 +25,51 @@ describe('WalletService', function () {
         done();
       });
     });
+
+    it('Should import a user with a custom programId', function (done) {
+      // This test passes because object 7 has `config.wallet.programId` set to "9182"
+      var walletService = sails.services.walletservice,
+          credentials = {
+            username: 'mytest',
+            email   : 'fortress-test+program-id@ratus.nl',
+            password: 'foobar',
+            object  : 7,
+            from_url: 'test.net',
+            ip      : '127.0.0.1',
+            p       : 123,
+            pi      : 'testing'
+          };
+
+      walletService.importUser(credentials, function (error, imported) {
+        assert.isNull(error);
+        assert.isNotNull(imported);
+        assert.isObject(imported);
+
+        done();
+      });
+    });
+
+    it('Should not find a user with a custom programId using the default object', function (done) {
+      // This test fails because object 1 doesn't have `config.wallet.programId` set to "9182"
+      var walletService = sails.services.walletservice,
+          credentials = {
+            username: 'mytest',
+            email   : 'fortress-test+program-id@ratus.nl',
+            password: 'foobar',
+            object  : 1,
+            from_url: 'test.net',
+            ip      : '127.0.0.1',
+            p       : 123,
+            pi      : 'testing'
+          };
+
+      walletService.importUser(credentials, function (error, imported) {
+        assert.isNull(error);
+        assert.isNull(imported);
+
+        done();
+      });
+    });
   });
 
   describe('.login()', function () {
